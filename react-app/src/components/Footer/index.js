1e68b8d20eb1c5a06b41891321014b468976d8c8
Michael <michael.oyola@outlook.com>
2023-08-31 02:36:44 -0400

    added footer

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -1,56 +1,77 @@
 
 import React, { useState, useEffect } from "react";
+import './footer.css'
 
 
-
-export default function Footer(){
+export default function Footer() {
 
 
     return (
-        <>
-        </>
-//         Shop and Learn
-// Store
-// Mac
-// iPad
-// iPhone
-// Watch
-// Vision
-// AirPods
-// TV & Home
-// AirTag
-// Accessories
-// Gift Cards
-// Apple Wallet
-// Wallet
-// Apple Card
-// Apple Pay
-// Apple Cash
-// Account
-// Manage Your Apple ID
-// Apple Store Account
-// iCloud.com
-// Entertainment
-// Apple One
-// Apple TV+
-// Apple Music
-// Apple Arcade
-// Apple Fitness+
-// Apple News+
-// Apple Podcasts
-// Apple Books
-// App Store
-// Apple Store
-// Find a Store
-// Genius Bar
-// Today at Apple
-// Apple Camp
-// Apple Store App
-// Certified Refurbished
-// Apple Trade In
-// Financing
-// Carrier Deals at Apple
-// Order Status
-// Shopping Help
+        <div className="MainContainerHoldingAllFooter">
+            <div>Developed By</div>
+            <div className="MainContainerForFooterIcons">
+                <div className="HoldingTitleAndLinks">
+                    <div className="ALinksTitle">Shop and Learn</div>
+                    <div>Store</div>
+                    <div>Mac</div>
+                    <div>iPad</div>
+                    <div>iPhone</div>
+                    <div>Watch</div>
+                    <div>Vision</div>
+                    <div>AirPods</div>
+                    <div>TV & Home</div>
+                    <div>AirTag</div>
+                    <div>Accessories</div>
+                    <div>Gift Cards</div>
+                </div>
+
+                <div className="HoldingTitleAndLinks ">
+                    <div className="HoldingTitleAndLinks ">
+                        <div className="ALinksTitle">Apple Wallet</div>
+                        <div>Wallet</div>
+                        <div>Apple Card</div>
+                        <div>Apple Pay</div>
+                        <div>Apple Cash</div>
+                    </div>
+
+                    <div className="HoldingTitleAndLinks account">
+                        <div className="ALinksTitle">Account</div>
+                        <div>Manage Your Apple ID</div>
+                        <div>Apple Store Account</div>
+                        <div>iCloud.com</div>
+                    </div>
+                </div>
+
+                <div className="HoldingTitleAndLinks">
+                    <div className="ALinksTitle">Entertainment</div>
+                    <div>Apple One</div>
+                    <div>Apple TV+</div>
+                    <div>Apple Music</div>
+                    <div>Apple Arcade</div>
+                    <div>Apple Fitness+</div>
+                    <div>Apple News+</div>
+                    <div>Apple Podcasts</div>
+                    <div>Apple Books</div>
+                    <div>App Store</div>
+                </div>
+
+                <div className="HoldingTitleAndLinks">
+                    <div className="ALinksTitle">Apple Store</div>
+                    <div>Find a Store</div>
+                    <div>Genius Bar</div>
+                    <div>Today at Apple</div>
+                    <div>Apple Camp</div>
+                    <div>Apple Store App</div>
+                    <div>Certified Refurbished</div>
+                    <div>Apple Trade In</div>
+                    <div>Financing</div>
+                    <div>Carrier Deals at Apple</div>
+                    <div>Order Status</div>
+                    <div>Shopping Help</div>
+                </div>
+            </div>
+        </div>
+
+
     )
 }
