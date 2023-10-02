0a9837e3d0277a14a0dcf5f2b56ffe4bed760259
Michael <Michael_oyola@outlook.com>
2023-10-02 15:59:51 -0400

    trying to do o auth

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request, session, redirect
+from flask import Flask, abort, request, session, redirect
 from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect, generate_csrf
@@ -16,7 +16,42 @@ from .api.save_routes import save_routes
 from .seeds import seed_commands
 from .config import Config
 
+# # OAuth imports
+import pathlib
+import requests
+from google.oauth2 import id_token
+from google_auth_oauthlib.flow import Flow
+from pip._vendor import cachecontrol
+import google.auth.transport.requests
+
 app = Flask(__name__, static_folder='../react-app/build', static_url_path='/')
+
+# OAuth = Flask("Google Login App")
+app.secret_key = os.environ.get('GOOGLE_OAUTH_CLIENT_SECRET') # make sure this matches with what's in client_secret.json
+
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1" # to allow Http traffic for local dev
+
+GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_OAUTH_CLIENT_ID')
+client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "client_secret.json")
+
+flow = Flow.from_client_secrets_file(
+    client_secrets_file=client_secrets_file,
+    scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "openid"],
+    redirect_uri=["http://127.0.0.1:5000/auth_routes/callback"]
+)
+
+
+def login_is_required(function):
+    def wrapper(*args, **kwargs):
+        if "google_id" not in session:
+            return abort(401)  # Authorization required
+        else:
+            return function()
+
+    return wrapper
+
+
+
 
 # Setup login manager
 login = LoginManager(app)
@@ -101,3 +136,76 @@ def react_root(path):
 @app.errorhandler(404)
 def not_found(e):
     return app.send_static_file('index.html')
+
+
+
+@app.route("/login")
+def login():
+    authorization_url, state = flow.authorization_url()
+    print("AUTH URL: ", authorization_url) # I recommend that you print this value out to see what it's generating.
+    # Ex: https://accounts.google.com/o/oauth2/auth?response_type=code&client_id=NICE TRY&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fcallback&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email+openid&state=A0eZyFD4WH6AfqSj7XcdypQ0cMhwr9&access_type=offline
+    # It SHOULD look a lot like the URL in the SECOND or THIRD line of our flow chart!
+    # Note that in the auth url above the value 'access_type' is set to 'offline'. If you do not send this, the user will NOT see the Google Login screen!!
+    # Additionally, note that this URL does NOT contain the 'code_challenge_method' value NOR the 'code_challenge' that can be seen in our flow chart.
+    # This package may have been created BEFORE the official Oauth2 consortium began recommending PKCE even for back channel flows...
+    # While implementation details are completely obscured by the method .authorization_url() let's note 2 things here.
+    # 1) We ARE generating a random value for the 'state' variable. We save it to the session on the line below to compare later.
+    # 2) The authorization URL
+    #print("STATE: ", state)
+    session["state"] = state
+    return redirect(authorization_url) # This line technically will enact the SECOND and THIRD lines of our flow chart.
+
+
+@app.route("/callback")
+def callback():
+    flow.fetch_token(authorization_response=request.url) # This method is sending the request depicted on line 6 of our flow chart! The response is depicted on line 7 of our flow chart.
+    # I find it odd that the author of this code is verifying the 'state' AFTER requesting a token, but to each their own!!
+
+    # This is our CSRF protection for the Oauth Flow!
+    if not session["state"] == request.args["state"]:
+        abort(500)  # State does not match!
+
+    credentials = flow.credentials
+    request_session = requests.session()
+    cached_session = cachecontrol.CacheControl(request_session)
+    token_request = google.auth.transport.requests.Request(session=cached_session)
+
+    # The method call below will go through the tedious work of verifying the JWT signature sent back with the object from OpenID Connect
+    # Although I cannot verify, hopefully it is also testing the values for "sub", "aud", "iat", and "exp" sent back in the CLAIMS section of the JWT
+    # Additionally note, that the oauth initializing URL generated in the previous endpoint DID NOT send a random nonce value. (As depicted in our flow chart)
+    # If it had, the server would return the nonce in the JWT claims to be used for further verification tests!
+    id_info = id_token.verify_oauth2_token(
+        id_token=credentials._id_token,
+        request=token_request,
+        audience=GOOGLE_CLIENT_ID
+    )
+
+    # Now we generate a new session for the newly authenticated user!!
+    # Note that depending on the way your app behaves, you may be creating a new user at this point...
+    session["google_id"] = id_info.get("sub")
+    session["name"] = id_info.get("name")
+
+    return redirect("/protected_area") #
+
+
+
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/")
+
+
+# @OAuth.route("/")
+# def index():
+#     return "Hello World <a href='/login'><button>Login</button></a>"
+
+# This is the endpoint that our final redirect is sending the GET request to.
+@app.route("/protected_area")
+@login_is_required
+def protected_area():
+    return f"Hello {session['name']}! <br/> <a href='/logout'><button>Logout</button></a>"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80, debug=True)
